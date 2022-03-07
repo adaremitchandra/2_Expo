3ab7fab085e950c2236787a5a895307bc3ef39fd
adaremitchandra <adaremitchandra@gmail.com>
2022-03-07 11:50:02 +0700

    fav

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,37 +1,18 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { CATEGORIES, MEALS } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+import React from "react";
+import { StyleSheet } from "react-native";
+import MealList from "../components/MealList";
+import { MEALS } from "../data/dummy-data";
 
 const CategoryMealsScreen = ({ route, navigation }) => {
-  const renderMealItem = (itemData) => {
-    return (
-      <MealItem
-        title={itemData.item.title}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        image={itemData.item.imageUrl}
-        onSelectMeal={() => {
-          navigation.navigate('MealDetail', itemData.item);
-        }}
-      />
-    );
-  };
-
   const catId = route.params.categoryId;
 
-  const displayedMeal = MEALS.filter((meal) => meal.categoryIds.indexOf(catId) >= 0);
-
-  return (
-    <View style={styles.screen}>
-      <FlatList data={displayedMeal} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
-    </View>
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
+
+  return <MealList data={displayedMeals} navigation={navigation} />;
 };
 
 export default CategoryMealsScreen;
 
-const styles = StyleSheet.create({
-  screen: { flex: 1, padding: 15, backgroundColor: 'white' },
-});
+const styles = StyleSheet.create({});
